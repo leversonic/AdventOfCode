e97452cf47f1a985cae5cd31ad484f12b74ffbbe
Alex Leversen <leversonic@gmail.com>
2024-12-01 14:47:11 -0500

    Implements day 1 part 2

--- Solvers/2024/ListDistanceSolver.cs
+++ Solvers/2024/ListDistanceSolver.cs
@@ -6,29 +6,31 @@ public partial class ListDistanceSolver : ISolver
 {
     public object Solve(string[] lines, int part)
     {
-        if (part == 1)
+        var lineRegex = LineRegex();
+        var lineValues = lines.Select(line =>
         {
-            var lineRegex = LineRegex();
-            var lineValues = lines.Select(line =>
-            {
-                var match = lineRegex.Match(line);
-                return (match.Groups[1].Value, match.Groups[2].Value);
-            }).ToList();
+            var match = lineRegex.Match(line);
+            return (match.Groups[1].Value, match.Groups[2].Value);
+        }).ToList();
 
-            var (leftValues, rightValues) = (lineValues.Select(v => int.Parse(v.Item1)).Order().ToList(),
-                lineValues.Select(v => int.Parse(v.Item2)).Order().ToList());
-            var total = 0;
-            for (var i = 0; i < leftValues.Count; i++)
+        var (leftValues, rightValues) = (lineValues.Select(v => int.Parse(v.Item1)).Order().ToList(),
+            lineValues.Select(v => int.Parse(v.Item2)).Order().ToList());
+        var total = 0;
+        for (var i = 0; i < leftValues.Count; i++)
+        {
+            var left = leftValues[i];
+            var right = rightValues[i];
+            if (part == 1)
             {
-                var left = leftValues[i];
-                var right = rightValues[i];
                 total += Math.Abs(left - right);
             }
-
-            return total;
+            else
+            {
+                total += left * rightValues.Count(v => v == left);
+            }
         }
 
-        return null!;
+        return total;
     }
 
     [GeneratedRegex(@"^\s*(\d+)\s*(\d+)$")]
