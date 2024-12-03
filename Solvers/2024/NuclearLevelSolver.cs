1742b94e620cb27fbd8d928f9e8ddb6c145883da
Alex Leversen <leversonic@gmail.com>
2024-12-02 22:26:45 -0500

    Implements Day 2 Part 2

--- Solvers/2024/NuclearLevelSolver.cs
+++ Solvers/2024/NuclearLevelSolver.cs
@@ -4,35 +4,43 @@ public class NuclearLevelSolver : ISolver
 {
     public object Solve(string[] lines, int part)
     {
-        var values = lines.Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
+        var values = lines.Select(line => line.Split(" ").Select(int.Parse).ToList()).ToArray();
         if (part == 1)
         {
-            return values.Count(value =>
-            {
-                var sign = 0;
-                for (var i = 0; i < value.Length - 1; i++)
-                {
-                    var difference = value[i + 1] - value[i];
-                    var newSign = difference < 0 ? -1 : 1;
-                    if (sign != 0 && newSign != sign)
-                    {
-                        return false;
-                    }
-
-                    if (Math.Abs(difference) is < 1 or > 3)
-                    {
-                        return false;
-                    }
-
-                    sign = newSign;
-                }
-
-                return true;
-            });
+            return values.Count(IsRowSafe);
         }
-        else
+
+        return values.Count(row =>
         {
-            return null!;
+            return row.Select((_, index) =>
+            {
+                var copy = row.Select(v => v).ToList();
+                copy.RemoveAt(index);
+                return copy;
+            }).Any(IsRowSafe);
+        });
+    }
+
+    private static bool IsRowSafe(List<int> row)
+    {
+        var sign = 0;
+        for (var i = 0; i < row.Count - 1; i++)
+        {
+            var difference = row[i + 1] - row[i];
+            var newSign = difference < 0 ? -1 : 1;
+            if (sign != 0 && newSign != sign)
+            {
+                return false;
+            }
+
+            if (Math.Abs(difference) is < 1 or > 3)
+            {
+                return false;
+            }
+
+            sign = newSign;
         }
+
+        return true;
     }
 }
