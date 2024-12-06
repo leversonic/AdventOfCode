c5d14395c666ef9bc0fd9471f5cec34f7e6aa05f
Alex Leversen <leversonic@gmail.com>
2024-12-05 23:39:00 -0500

    Implements Day 5 Part 2

--- Solvers/2024/SafetyManualPageSolver.cs
+++ Solvers/2024/SafetyManualPageSolver.cs
@@ -12,26 +12,59 @@ public class SafetyManualPageSolver : ISolver
             return (splitLine[0], splitLine[1]);
         }).ToArray();
         var pageLines = lines[(breakIndex + 1)..].Select(line => line.Split(',').ToList()).ToList();
-        if (part == 1)
-        {
             foreach (var pageLine in pageLines)
             {
                 var lineIsValid = ApplyRules(ruleLines, pageLine);
-                if (lineIsValid)
+                switch (part)
                 {
-                    total += int.Parse(pageLine[pageLine.Count / 2]);
+                    case 2 when !lineIsValid:
+                    {
+                        var finalPageLine = FixLine(ruleLines, pageLine);
+                        total += int.Parse(finalPageLine[finalPageLine.Count / 2]);
+                        break;
+                    }
+                    case 1 when lineIsValid:
+                        total += int.Parse(pageLine[pageLine.Count / 2]);
+                        break;
                 }
             }
-        }
 
         return total;
     }
 
     private static bool ApplyRules((string, string)[] ruleLines, List<string> pageLine) =>
-        ruleLines.All(ruleLine =>
+        ruleLines.All(ruleLine => ApplyRule(ruleLine, pageLine));
+
+    private static List<string> FixLine((string, string)[] ruleLines, List<string> pageLine)
+    {
+        var result = pageLine.Select(l => l).ToList();
+        while (true)
         {
-            var firstItemIndex = pageLine.IndexOf(ruleLine.Item1);
-            var secondItemIndex = pageLine.IndexOf(ruleLine.Item2);
-            return firstItemIndex == -1 || secondItemIndex == -1 || firstItemIndex < secondItemIndex;
-        });
+            var isFixed = true;
+            foreach (var ruleLine in ruleLines)
+            {
+                if (ApplyRule(ruleLine, result))
+                {
+                    continue;
+                }
+                isFixed = false;
+                result.RemoveAt(result.IndexOf(ruleLine.Item2));
+                result.Insert(result.IndexOf(ruleLine.Item1) + 1, ruleLine.Item2);
+            }
+
+            if (isFixed)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private static bool ApplyRule((string, string) ruleLine, List<string> pageLine)
+    {
+        var firstItemIndex = pageLine.IndexOf(ruleLine.Item1);
+        var secondItemIndex = pageLine.IndexOf(ruleLine.Item2);
+        return firstItemIndex == -1 || secondItemIndex == -1 || firstItemIndex < secondItemIndex;
+    }
 }
