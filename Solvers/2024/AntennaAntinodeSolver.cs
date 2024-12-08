145ab6e59a5d7c13865e3fcc15842b64ad2926f5
Alex Leversen <leversonic@gmail.com>
2024-12-08 18:28:10 -0500

    Implements Day 8 Part 2

--- Solvers/2024/AntennaAntinodeSolver.cs
+++ Solvers/2024/AntennaAntinodeSolver.cs
@@ -18,32 +18,65 @@ public partial class AntennaAntinodeSolver : ISolver
             .ToList();
         var antinodes = new HashSet<(int x, int y)>();
 
-        var result = 0;
-        if (part == 1)
-        {
             foreach (var location in antennaLocations)
             {
                 var otherLocations = antennaLocations
                     .Where(l => l != location && l.c == location.c);
                 foreach (var otherLocation in otherLocations)
                 {
-                    var difference = (location.x - otherLocation.x, location.y - otherLocation.y);
-                    (int x, int y) antinode1 = (location.x + difference.Item1, location.y + difference.Item2);
-                    (int x, int y) antinode2 = (otherLocation.x - difference.Item1, otherLocation.y - difference.Item2);
-                    new[] { antinode1, antinode2 }
-                        .Where(InRange)
-                        .ToList()
-                        .ForEach(antinode => antinodes.Add(antinode));
+                    var difference = (x: location.x - otherLocation.x, y: location.y - otherLocation.y);
+                    if (part == 1)
+                    {
+                        var antinode1 = (x: location.x + difference.x, y: location.y + difference.y);
+                        var antinode2 = (x: otherLocation.x - difference.x, y: otherLocation.y - difference.y);
+                        new[] { antinode1, antinode2 }
+                            .Where(InRange)
+                            .ToList()
+                            .ForEach(antinode => antinodes.Add(antinode));
+                    }
+                    else
+                    {
+                        var (xFactor, yFactor) = DivideByLeastCommonPrimeFactor(difference.x, difference.y);
+                        var multiple = 0;
+                        while (true)
+                        {
+                            var positiveLocationToTest = (x: otherLocation.x + xFactor * multiple, y: otherLocation.y + yFactor * multiple);
+                            var negativeLocationToTest = (x: location.x - xFactor * multiple, y: location.y - yFactor * multiple);
+                            var positiveInRange = InRange(positiveLocationToTest);
+                            var negativeInRange = InRange(negativeLocationToTest);
+                            if (positiveInRange)
+                            {
+                                antinodes.Add(positiveLocationToTest);
+                            }
+
+                            if (negativeInRange)
+                            {
+                                antinodes.Add(negativeLocationToTest);
+                            }
+
+                            multiple++;
+                            if (!positiveInRange && !negativeInRange)
+                            {
+                                break;
+                            }
+                        }
+                    }
                 }
             }
 
-            result = antinodes.Count;
-        }
-
-        return result;
+        return antinodes.Count;
 
         bool InRange((int x, int y) antinode) => 
             antinode.x >= 0 && antinode.x < lines[0].Length && antinode.y >= 0 && antinode.y < lines.Length;
+
+        (int n1, int n2) DivideByLeastCommonPrimeFactor(int n1, int n2)
+        {
+            for (var i = 2; i <= n1; i++)
+            {
+                if (n1 % i == 0 && n2 % i == 0) return (n1 / i, n2 / i);
+            }
+            return (n1, n2);
+        }
     }
 
     [GeneratedRegex("[a-zA-Z0-9]{1}")]
