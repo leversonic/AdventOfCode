b5b521aaf1a64d2ff972bad2f0c13f91964bd7e2
Alex Leversen <leversonic@gmail.com>
2024-12-10 21:53:42 -0500

    Implements Day 10 Part 2

--- Solvers/2024/HikingTrailSolver.cs
+++ Solvers/2024/HikingTrailSolver.cs
@@ -12,19 +12,16 @@ public class HikingTrailSolver : ISolver
                 .Select((_, x) => (x, y)))
             .Where(tuple => map[tuple.y][tuple.x] == 0);
         var score = 0;
-        if (part == 1)
+        foreach (var trailhead in trailheads)
         {
-            foreach (var trailhead in trailheads)
-            {
-                _visitedPeaks = new List<(int x, int y)>();
-                score += CalculateScore(map, trailhead.x, trailhead.y);
-            }
+            _visitedPeaks = [];
+            score += CalculateScore(map, trailhead.x, trailhead.y, part == 1);
         }
 
         return score;
     }
 
-    private int CalculateScore(int[][] map, int x, int y)
+    private int CalculateScore(int[][] map, int x, int y, bool trackVisited)
     {
         var currentValue = map[y][x];
         var score = 0;
@@ -32,48 +29,48 @@ public class HikingTrailSolver : ISolver
         {
             if (map[y][x - 1] == currentValue + 1)
             {
-                if (currentValue == 8 && !_visitedPeaks.Contains((x - 1, y)))
+                if (currentValue == 8 && (!trackVisited || !_visitedPeaks.Contains((x - 1, y))))
                 {
                     _visitedPeaks.Add((x - 1, y));
                     score += 1;
                 }
-                score += CalculateScore(map, x - 1, y);
+                score += CalculateScore(map, x - 1, y, trackVisited);
             }
         }
         if (x < map.Length - 1)
         {
             if (map[y][x + 1] == currentValue + 1)
             {
-                if (currentValue == 8 && !_visitedPeaks.Contains((x + 1, y)))
+                if (currentValue == 8 && (!trackVisited || !_visitedPeaks.Contains((x + 1, y))))
                 {
                     _visitedPeaks.Add((x + 1, y));
                     score += 1;
                 }
-                score += CalculateScore(map, x + 1, y);
+                score += CalculateScore(map, x + 1, y, trackVisited);
             }
         }
         if (y > 0)
         {
             if (map[y - 1][x] == currentValue + 1)
             {
-                if (currentValue == 8 && !_visitedPeaks.Contains((x, y - 1)))
+                if (currentValue == 8 && (!trackVisited || !_visitedPeaks.Contains((x, y - 1))))
                 {
                     _visitedPeaks.Add((x, y - 1));
                     score += 1;
                 }
-                score += CalculateScore(map, x, y - 1);
+                score += CalculateScore(map, x, y - 1, trackVisited);
             }
         }
         if (y < map.Length - 1)
         {
             if (map[y + 1][x] == currentValue + 1)
             {
-                if (currentValue == 8 && !_visitedPeaks.Contains((x, y + 1)))
+                if (currentValue == 8 && (!trackVisited || !_visitedPeaks.Contains((x, y + 1))))
                 {
                     _visitedPeaks.Add((x, y + 1));
                     score += 1;
                 }
-                score += CalculateScore(map, x, y + 1);
+                score += CalculateScore(map, x, y + 1, trackVisited);
             }
         }
         
