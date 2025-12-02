8cbac4ea71f58b8e5547807331fd67eb1124ca8c
Alex Leversen <leversonic@gmail.com>
2025-12-01 19:52:33 -0500

    Updates solution to use latest .NET

--- Utility/Extensions.cs
+++ Utility/Extensions.cs
@@ -2,6 +2,6 @@ namespace AdventOfCode.Utility;
 
 public static class Extensions {
     public static string Reverse(this string str) {
-        return new string(str.Reverse<char>().ToArray());
+        return new(str.Reverse<char>().ToArray());
     }
 }
