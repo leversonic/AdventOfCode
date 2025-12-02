691736e5d6b2e3d9268166326544c63ada297ded
Alex Leversen <leversonic@gmail.com>
2025-12-01 21:10:36 -0500

    Reformats program file

--- Program.cs
+++ Program.cs
@@ -10,32 +10,39 @@ public static class Program
         }
 
         var argIndices = args
-        .Select((a, i) => (arg: a, index: i))
-        .Where(tuple => tuple.arg.StartsWith("--"));
+            .Select((a, i) => (arg: a, index: i))
+            .Where(tuple => tuple.arg.StartsWith("--"));
 
         var year = -1;
         var day = -1;
         var part = -1;
         var testMode = false;
 
-        foreach(var (argName, index) in argIndices) {
-            switch(argName) {
+        foreach (var (argName, index) in argIndices)
+        {
+            switch (argName)
+            {
                 case "--test":
                     testMode = true;
                     continue;
             }
+
             var argIndex = index + 1;
-            if (argIndex >= args.Length) {
+            if (argIndex >= args.Length)
+            {
                 PrintUsageStringAndExit();
             }
+
             var argValue = args[argIndex];
-            switch(argName) {
+            switch (argName)
+            {
                 case "--year":
                     if (!int.TryParse(argValue, out year))
                     {
                         PrintUsageStringAndExit();
                         return;
                     }
+
                     break;
                 case "--day":
                     if (!int.TryParse(argValue, out day))
@@ -43,6 +50,7 @@ public static class Program
                         PrintUsageStringAndExit();
                         return;
                     }
+
                     break;
                 case "--part":
                     if (!int.TryParse(argValue, out part))
@@ -50,6 +58,7 @@ public static class Program
                         PrintUsageStringAndExit();
                         return;
                     }
+
                     break;
             }
         }
@@ -60,40 +69,51 @@ public static class Program
             Console.WriteLine("Error: missing value for required parameter --year");
             missingParameter = true;
         }
-        if (day == -1) {
+
+        if (day == -1)
+        {
             Console.WriteLine("Error: missing value for required parameter --day");
             missingParameter = true;
         }
-        if (part == -1) {
+
+        if (part == -1)
+        {
             Console.WriteLine("Error: missing value for required parameter --part");
             missingParameter = true;
         }
-        if (missingParameter) {
+
+        if (missingParameter)
+        {
             PrintUsageStringAndExit();
         }
 
         var lines = File.ReadAllLines($"./input/{year:D}/{day:D2}{GetTestString()}.txt");
 
-        try {
+        try
+        {
             var solver = SolverDict.Solvers[year][day];
             var solution = solver.Solve(lines, part);
 
             Console.WriteLine($"Solution: {solution}");
-        } catch (KeyNotFoundException) {
-            Console.WriteLine($"Error: solution for Day {day} Part {part} either has not been implemented or has not been added to SolverDict.");
+        }
+        catch (KeyNotFoundException)
+        {
+            Console.WriteLine(
+                $"Error: solution for Day {day} Part {part} either has not been implemented or has not been added to SolverDict.");
         }
 
         return;
 
         string GetTestString() => testMode ? "-test" : "";
     }
-    private static void PrintUsageStringAndExit()
-        {
-            Console.WriteLine("""
-        Usage:
 
-        ./run.sh --day <day> --part <part>
-        """);
+    private static void PrintUsageStringAndExit()
+    {
+        Console.WriteLine("""
+                          Usage:
+
+                          ./run.sh --day <day> --part <part> --year <year> (--test)
+                          """);
         Environment.Exit(-1);
-        }
+    }
 }
