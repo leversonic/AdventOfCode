afbf51ef9d95b58f387a203bd827b57f83210db5
Alex Leversen <leversonic@gmail.com>
2025-12-03 21:59:42 -0500

    Implements Day 3 Part 2.

--- Solvers/2025/JoltageSolver.cs
+++ Solvers/2025/JoltageSolver.cs
@@ -1,30 +1,41 @@
-﻿namespace AdventOfCode.Solvers._2025;
+﻿using System.Numerics;
+
+namespace AdventOfCode.Solvers._2025;
 
 public class JoltageSolver : ISolver
 {
     public object Solve(string[] lines, int part)
     {
         var parsedLines = lines.Select(line => line.Select(c => int.Parse(c.ToString())));
-        var sum = 0;
+        var sum = BigInteger.Zero;
+        var digitCount = part switch
+        {
+            1 => 2,
+            2 => 12,
+            _ => throw new InvalidOperationException("Invalid part argument encountered")
+        };
         foreach (var parsedLine in parsedLines)
         {
             var parsedLineList = parsedLine.ToList();
-            var largestNumberInfo = GetLargestNumberInfo(parsedLineList);
-            if (largestNumberInfo.Index == parsedLineList.Count - 1)
+            var remainingDigitsCount = digitCount - 1;
+            var currentDigits = new List<int>();
+            while (remainingDigitsCount >= 0)
             {
-                largestNumberInfo = GetLargestNumberInfo(parsedLineList[..^1]);
+                var nextDigitInfo = GetLargestNumberInfo(parsedLineList, remainingDigitsCount);
+                currentDigits.Add(nextDigitInfo.Value);
+                parsedLineList = parsedLineList[(nextDigitInfo.Index + 1)..];
+                remainingDigitsCount--;
             }
 
-            var secondLargestNumberInfo = GetLargestNumberInfo(parsedLineList[(largestNumberInfo.Index + 1)..]);
-
-            var combinedNumber =
-                int.Parse($"{largestNumberInfo.Value.ToString()}{secondLargestNumberInfo.Value.ToString()}");
+            var combinedNumber = BigInteger.Parse(string.Join("", currentDigits));
             sum += combinedNumber;
         }
 
         return sum;
 
-        (int Value, int Index) GetLargestNumberInfo(IEnumerable<int> list) =>
-            list.Select((value, index) => (Value: value, Index: index)).MaxBy(tuple => tuple.Value);
+        (int Value, int Index) GetLargestNumberInfo(IEnumerable<int> list, int digitsToOmit) =>
+            list.ToList()[..^digitsToOmit]
+                .Select((value, index) => (Value: value, Index: index))
+                .MaxBy(tuple => tuple.Value);
     }
 }
